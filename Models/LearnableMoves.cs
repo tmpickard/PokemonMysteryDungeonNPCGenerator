5d3c38764e1184f05566b38f1a232dd76de7bfa0
Ted Pickard <teddy.pickard@pickard-murphy.com>
2021-08-01 17:52:57 -0700

    Added comments for joining object.

--- Models/LearnableMoves.cs
+++ Models/LearnableMoves.cs
@@ -7,10 +7,10 @@ namespace PokemonMysteryDungeonNPCGenerator.Models
 {
     public class LearnableMoves
     {
-        public int LearnableMoveID { get; set; }
-        public int PokemonId { get; set; }
-        public int MoveId { get; set; }
-        public string LearnMethod { get; set; }
-        public int LearnLevel { get; set; }
+        public int LearnableMoveID { get; set; } //Primary Key
+        public int PokemonId { get; set; } //Foreign Key for Pokemon
+        public int MoveId { get; set; } //Foreign Key for specific Move
+        public string LearnMethod { get; set; } //States whether a pokemon learns a move by leveling up, TM, tutoring, egg move, etc.
+        public int LearnLevel { get; set; } = 1; //Defaults as 1 unless otherwise stated.
     }
 }
